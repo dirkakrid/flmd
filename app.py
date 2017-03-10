9442363e0bdad59c9ef35ec010b3f2a8cb752c6b
Harry Park <harry@harrypark.io>
2017-03-10 20:40:49 +1100

    404

--- app.py
+++ app.py
@@ -24,11 +24,11 @@ class config:
 		else:
 			self.data = data
 
-def get_md(file):
+def get_md_from_file(file):
 	if os.path.isfile(file):
 		with open(file, 'r') as file_obj:
 			file_contents = file_obj.read()
-		return Markup(markdown.markdown(file_contents))
+		return markdown.markdown(file_contents)
 
 @app.route('/')
 @app.route('/<path:file>')
@@ -36,17 +36,19 @@ def index(file='/'):
 	file_name = config('content_dir').data + '{}' + config('ext').data
 	output = None
 	if file.endswith('/'):
-		output = get_md(file_name.format(file + 'index'))
+		output = get_md_from_file(file_name.format(file + 'index'))
 	else:
-		output = get_md(file_name.format(file))
+		output = get_md_from_file(file_name.format(file))
 		if output == None:
-			output = get_md(file_name.format(file + '/index'))
-	return render_template('index.html', content=output) if output is not None else abort(404)
+			output = get_md_from_file(file_name.format(file + '/index'))
+	return render_template('index.html', content=Markup(output)) if output is not None else abort(404)
 
 
 @app.errorhandler(404)
 def err_404(e):
-	return '404', 404
+	content = get_md_from_file(config('content_dir').data + '404' + config('ext').data)
+	output = render_template('index.html', content=Markup(content))
+	return output if content is not None else '404'
 
 if __name__ == "__main__":
 	app.run()
