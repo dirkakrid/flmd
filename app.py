587674571b26d0385906debee468281d94a1485a
Harry Park <harry@harrypark.io>
2017-03-11 08:53:13 +1100

    config + oop

--- app.py
+++ app.py
@@ -6,6 +6,17 @@ app = Flask(__name__)
 app.threaded = True
 app.debug = True
 
+def is_file(file_path):
+	if os.path.isfile(file_path):
+		return True
+	return False
+
+def rrmadd(string, delim):
+	return string.rstrip(delim) + delim
+
+def lrmadd(string, delim):
+	return delim + string.lstrip(delim)
+
 class config:
 	def __init__(self, val=None, file='config.json', delim='.'):
 		with open(file) as f:
@@ -24,29 +35,40 @@ class config:
 		else:
 			self.data = data
 
-def get_md_from_file(file):
-	if os.path.isfile(file):
-		with open(file, 'r') as file_obj:
-			file_contents = file_obj.read()
-		return markdown.markdown(file_contents)
+class theme:
+	def __init__(self):
+		self.theme_dir = rrmadd(config('theme.dir').data, '/')
+		self.current_theme = config('theme.current').data
+
+	def get_template(self, template):
+		if is_file(self.theme)
+
+class file_parser:
+	def __init__(self, file_name):
+		self.file_name = file_name
+
+	def get_md_from_file(self):
+		if is_file(self.file_name):
+			with open(file, 'r') as file_obj:
+				file_contents = file_obj.read()
+			return markdown.markdown(file_contents)
 
 @app.route('/')
 @app.route('/<path:file>')
 def index(file='/'):
-	file_name = config('content_dir').data + '{}' + config('ext').data
+	file_name = rrmadd(config('content.dir').data, '/') + '{}' + lrmadd(config('content.ext').data, '.')
 	output = None
 	if file.endswith('/'):
-		output = get_md_from_file(file_name.format(file + 'index'))
+		output = file_parser(file_name.format(file + 'index')).get_md_from_file()
 	else:
-		output = get_md_from_file(file_name.format(file))
+		output = file_parser(file_name.format(file)).get_md_from_file()
 		if output == None:
-			output = get_md_from_file(file_name.format(file + '/index'))
+			output = file_parser(file_name.format(file + '/index')).get_md_from_file()
 	return render_template('index.html', content=Markup(output)) if output is not None else abort(404)
-
 
 @app.errorhandler(404)
 def err_404(e):
-	content = get_md_from_file(config('content_dir').data + '404' + config('ext').data)
+	content = file_parser(config('content_dir').data + '404' + config('ext').data).get_md_from_file()
 	output = render_template('index.html', content=Markup(content))
 	return output if content is not None else '404'
 
